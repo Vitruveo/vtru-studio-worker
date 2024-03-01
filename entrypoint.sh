28020efa6e8b528077cc7277e013b8bdcd066aa7
Nelio Santos <nsfilho@icloud.com>
2024-03-01 14:09:17 -0300

    fix(core): fixing tools

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 function checkEnv() {
 	envName=$1
@@ -9,27 +9,23 @@ function checkEnv() {
 	fi
 }
 
-if [ "x$@" = "xwait" ] ; then
-    if [ "x$RABBITMQ_PORT" = "x" ] ; then
-        export RABBITMQ_PORT=5672
-    fi
-    if [ "x$RABBITMQ_HOST" = "x" ] ; then
-        echo RABBITMQ_HOST não definido
-    else
-        node tools/wait.js $RABBITMQ_HOST $RABBITMQ_PORT
-    fi
-    if [ "x$NODE_ENV" = "xproduction" ] ; then
-        while true ; do
-            date
-            npm start
-            sleep 300
-        done
-    else
-        echo NODE_ENV já definido: $NODE_ENV
-        npm run $NODE_ENV
-    fi
+if [ "x$@" = "xwait" ]; then
+	checkEnv RABBITMQ_PORT 5672
+	checkEnv RABBITMQ_HOST rabbitmq
+	node tools/wait.js $RABBITMQ_HOST $RABBITMQ_PORT
+
+	if [ "x$NODE_ENV" = "xproduction" ]; then
+		while true; do
+			date
+			npm start
+			sleep 300
+		done
+	else
+		echo NODE_ENV já definido: $NODE_ENV
+		npm run $NODE_ENV
+	fi
 else
-    echo Executando comando: $@
-    $@
+	echo Executando comando: $@
+	$@
 fi
 sleep 60
